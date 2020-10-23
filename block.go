c742959a2b7ae748e62667d291b2f50e00496ca3
Mooje Kong <mooje.kong@weather.com>
2020-10-23 15:05:44 +0900

    add pow

--- block.go
+++ block.go
@@ -12,11 +12,12 @@ type Block struct {
 	Data 					[]byte
 	PrevBlockHash			[]byte
 	Hash 					[]byte
+	Nonce 					int
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
 
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
 	block.SetHash()
 
 	return block
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	bc := NewBlockchain()
@@ -10,9 +13,11 @@ func main() {
 
 
 	for _, block := range bc.blocks {
-		fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
